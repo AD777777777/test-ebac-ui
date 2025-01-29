8652618272efdae713e2faa271755e555b1109f4
AD <adnewrock@gmail.com>
2025-01-29 14:19:25 -0300

    meu primeiro commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,12 @@
+/// <reference types= "cypress" />
+
+context('Funcionalidade login', () =>{
+
+    it('Deve fazer login com sucesso', () => {
+
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuario ou senha inválidos', () => { 
+
+    })
+})
